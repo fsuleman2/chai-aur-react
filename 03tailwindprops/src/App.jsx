a30dc10409ed755df5fb64b8fd9d2a1724b1b051
Syed Suleman <sydsulmn7@gmail.com>
2024-11-22 19:32:05 +0530

    completed video07 props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./components/Card";
+
+export default function App() {
+  return (
+    <>
+    <h1 className="bg-green-500 text-5xl font-bold underline">
+      Hello world!
+    </h1>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    </>
+  )
+}
