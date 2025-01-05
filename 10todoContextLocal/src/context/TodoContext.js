399035697c6019a421cb6d3a4e6e6750c085a60d
Syed Suleman <sydsulmn7@gmail.com>
2025-01-05 19:46:07 +0530

    10todoContextLocal setup and logic

---
+++ 10todoContextLocal/src/context/TodoContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+  //model - Definiations
+  todos: [
+    {
+      id: 1,
+      todo: "Todo Msg",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+export const TodoProvider = TodoContext.Provider;
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+//Ek baat yaad rakhiye ga jab bhi useContext use karte hai, us ko Context dena padhta hai, batana padhta hai
+//kis context k barey mai baad kar rahe hai
